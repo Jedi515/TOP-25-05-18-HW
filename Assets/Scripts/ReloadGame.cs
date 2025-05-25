2faa4cf5218373c469adc4ae69c143dc6f73210d
Jedi515 <jedimailpost515@mail.ru>
2025-05-25 17:28:12 +1000

    Update ReloadGame.cs

--- Assets/Scripts/ReloadGame.cs
+++ Assets/Scripts/ReloadGame.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class ReloadGame : MonoBehaviour
 {
@@ -19,6 +20,7 @@ public class ReloadGame : MonoBehaviour
 
     public void Reload()
     {
-        Debug.Log("Reloading game");
+        //Debug.Log("Reloading game");
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
     }
 }
