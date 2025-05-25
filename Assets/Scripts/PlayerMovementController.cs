2cee145531fbc1690ba2380f44fdfa82259a33d0
Jedi515 <jedimailpost515@mail.ru>
2025-05-25 17:21:50 +1000

    +hw +basic ui

---
+++ Assets/Scripts/PlayerMovementController.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+[RequireComponent(typeof(CharacterController))]
+public class PlayerMovementController : MonoBehaviour
+{
+    [SerializeField]
+    private float maxRotateSpeed = 6.0f;
+    [SerializeField]
+    private float maxMoveSpeed = 10f;
+
+    public GameObject gameOverScreen;
+
+    private CharacterController characterController;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        characterController = GetComponent<CharacterController>();
+    }
+
+    private void FixedUpdate()
+    {
+        //transform.Rotate(0,  Input.GetAxis("Mouse X") * maxRotateSpeed, 0);
+        transform.Rotate(0, Input.GetAxis("Mouse X") * maxRotateSpeed, 0);
+        characterController?.Move(Vector3.ClampMagnitude(
+            new Vector3(
+                Input.GetAxis("Horizontal") * maxMoveSpeed * Time.deltaTime,
+                0,
+                Input.GetAxis("Vertical") * maxMoveSpeed * Time.deltaTime), maxMoveSpeed)
+        );
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void ReloadGame()
+    {
+        Debug.Log("Reloading game");
+    }
+}
